9d2b4bcab6bb75b0d38a3504277cd96a6f5f5f95
sanjayMishra1222 <sanjumishra14452@gmail.com>
2024-12-29 21:49:55 +0530

    Add files via upload

---
+++ SanjuJS/First.js
@@ -0,0 +1,46 @@
+// // console.log("Sanju mishra")
+// let a = 5
+// let b = 4
+// console.log(a + b)
+// console.log("the divde is :", a / b)
+// console.log(a - b)
+// console.log(a % b)
+// console.log(a * b)
+// console.log(a / b)
+// let a = 5;
+// let b = 4;
+// let age = 13;
+// if (age >= 18) {
+//     console.log("ypu are eligible to vote ")
+// } else {
+//     console.log("you are not eligible for vote ")
+// }
+// let b = 4;
+// console.log(" predicriment of 4 is :", --b)
+// console.log(b)
+
+// console.log("post Dicriment of 4 is : ", b--)
+// console.log(b)
+// console.log("post incriment of 4 is", b++)
+// console.log(b)
+// console.log("post inciment of 4 is :", ++b)
+// console.log(b)
+// console.log("SAnju Mishra")
+//     //Loopes in JS
+// for (let s = 0; s <= 6; s++) {
+//     console.log(" Hello May I Helpe You Sir ");
+//     console.log("My Name Is Sanju");
+// }
+// let add = 0;
+// for (let i = 0; i <= 5; i--) {
+//     add = add + i;
+
+// }
+// console.log("the sum of 5 integers is : ", add)
+// console.log("The Loop Is Ended")
+for (let i = 0; i <= 5; i++) {
+    // console.log(" May I Helpe You Sir ")
+
+
+}
+//console.log(" May I Helpe You Sir ");
